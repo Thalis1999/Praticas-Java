a239d76cab024e643393494a610c04ec13015656
occtober456@outlook.com <occtober456@outlook.com>
2025-10-04 18:02:24 -0300

    1

--- src/ultraemojicombat/Principal.java
+++ src/ultraemojicombat/Principal.java
@@ -4,23 +4,23 @@ public class Principal {
     static void main() {
         ultraemojicombat.Lutador l1[] = new Lutador[4];
 
-        l1[0] = new Lutador("Pretao", "França",
+        l1[0] = new Lutador("Prestes", "França",
                 31, 1.75f, 68.9f,
                 11, 3, 1);
 
-        l1[1] = new Lutador("Pretao", "Brasil",
+        l1[1] = new Lutador("Henk", "Brasil",
                 31, 1.75f, 68.9f,
                 11, 3, 1);
 
-        l1[2] = new Lutador("Pretao", "Mongolia",
+        l1[2] = new Lutador("Prela", "Mongolia",
                 32, 1.75f, 68.9f,
                 11, 3, 1);
 
-        l1[3] = new Lutador("Pretao", "Sudão",
+        l1[3] = new Lutador("Talis", "Sudão",
                 22, 1.77f, 68.8f,
                 11, 3, 1);
 
-        l1[4] = new Lutador("Pretao", "Itália",
+        l1[4] = new Lutador("Ane", "Itália",
                 21, 1.76f, 68.6f,
                 11, 3, 1);
         l1[0].status();
